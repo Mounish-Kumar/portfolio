499f71465c748c2f302828bbb1f5223f886947d3
Mounish-Kumar <vmounishkumar@gmail.com>
2020-06-30 01:01:29 +0530

    Removed parallax for mobile + Google analytics

--- app/src/scripts/parallax.js
+++ app/src/scripts/parallax.js
@@ -6,17 +6,18 @@ export var parallax = function() {
     let h1 = homeContent.getElementsByTagName("h1")[0];
     let h2 = homeContent.getElementsByTagName("h2")[0];
     let action = homeContent.getElementsByClassName("action")[0];
-    if(window.innerWidth <= 767) { // Parallax for mobile
-        // homeContent.style.top = 3 * 16 + scrollY * 0.8 + 'px';
-        // homeContent.style.transform = 'scale(' + (1 + scrollY / (764 * 1.6)) + ')';
-        // homeImage.style.bottom = 0 - scrollY * 0.8 + 'px';
-        // action.style.top = 0 - scrollY * 0.7 + 'px';
-        // action.style.transform = 'scale(' + (1 + scrollY / 764) + ')';
-    } else { // Parallax for tablet & desktop
+    if(window.innerWidth >= 768) { // Parallax for tablet & desktop
         homeImage.style.bottom = 0 - scrollY * 1.2 + 'px';
         homeImage.style.transform = 'scale(' + (1 + scrollY / (464 * 2)) + ')';
         homeContent.style.bottom = 0 - scrollY * 0.8 + 'px';
         h2.style.fontSize = (1.5 + scrollY / (464 * 1.2)) + 'rem';
         h1.style.fontSize = (2.5 + scrollY / 464) + 'rem';
     }
+    // else { // Parallax for mobile
+    //     homeContent.style.top = 3 * 16 + scrollY * 0.8 + 'px';
+    //     homeContent.style.transform = 'scale(' + (1 + scrollY / (764 * 1.6)) + ')';
+    //     homeImage.style.bottom = 0 - scrollY * 0.8 + 'px';
+    //     action.style.top = 0 - scrollY * 0.7 + 'px';
+    //     action.style.transform = 'scale(' + (1 + scrollY / 764) + ')';
+    // }
 }
